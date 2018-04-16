d91be3ec567ce83a79c296811845e1d2d9462f22
Birol Senturk <birolsenturk@birols-mbp.devices.brown.edu>
2018-04-16 15:24:17 -0400

    relative effectiveness and league tables are done (finallly). studies table is using DataTables now, so we can sort, search, hide columns and all that stuff

--- index.js
+++ index.js
@@ -1,16 +1,93 @@
 var element_json = {};
 var graph_data = {};
 var node_info = {};
+var relative_effectiveness_dict = { 'all': {}, 'stress': {}, 'urge': {}, 'ow': {} };
+var league_dict = { 'all': {}, 'stress': {}, 'urge': {}, 'ow': {} };
 
 const request = async () => {
-  const graph_data_response = await fetch('./data/studies.json');
-  graph_data = await graph_data_response.json();
+  
+  var response = await fetch('./data/studies.json');
+  graph_data = await response.json();
 
-  const node_info_response = await fetch('./data/node_info.json');
-  node_info = await node_info_response.json();
+  response = await fetch('./data/style.json');
+  node_info = await response.json();
 
-  const elements_response = await fetch('./data/elements.json');
-  elements = await elements_response.json();
+  response = await fetch('./data/elements.json');
+  elements = await response.json();
+
+  //get relative effectiveness tables
+  response = await fetch('./data/tables/relative-cure.html');
+  relative_effectiveness_dict['all']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/relative-improvement.html');
+  relative_effectiveness_dict['all']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/relative-satisfaction.html');
+  relative_effectiveness_dict['all']['satisfaction'] = await response.text();
+ 
+  response = await fetch('./data/tables/relative-cure-stress.html');
+  relative_effectiveness_dict['stress']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/relative-cure-urge.html');
+  relative_effectiveness_dict['urge']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/relative-cure-ow.html');
+  relative_effectiveness_dict['ow']['cure'] = await response.text();
+ 
+  response = await fetch('./data/tables/relative-improvement-stress.html');
+  relative_effectiveness_dict['stress']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/relative-improvement-urge.html');
+  relative_effectiveness_dict['urge']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/relative-improvement-ow.html');
+  relative_effectiveness_dict['ow']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/relative-satisfaction-stress.html');
+  relative_effectiveness_dict['all']['satisfaction'] = await response.text();
+
+  response = await fetch('./data/tables/relative-satisfaction-urge.html');
+  relative_effectiveness_dict['urge']['satisfaction'] = await response.text();
+
+  response = await fetch('./data/tables/relative-satisfaction-ow.html');
+  relative_effectiveness_dict['ow']['satisfaction'] = await response.text();
+ 
+  //get league tables
+  response = await fetch('./data/tables/league-cure.html');
+  league_dict['all']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/league-improvement.html');
+  league_dict['all']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/league-satisfaction.html');
+  league_dict['all']['satisfaction'] = await response.text();
+ 
+  response = await fetch('./data/tables/league-cure-stress.html');
+  league_dict['stress']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/league-cure-urge.html');
+  league_dict['urge']['cure'] = await response.text();
+
+  response = await fetch('./data/tables/league-cure-ow.html');
+  league_dict['ow']['cure'] = await response.text();
+ 
+  response = await fetch('./data/tables/league-improvement-stress.html');
+  league_dict['stress']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/league-improvement-urge.html');
+  league_dict['urge']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/league-improvement-ow.html');
+  league_dict['ow']['improvement'] = await response.text();
+
+  response = await fetch('./data/tables/league-satisfaction-stress.html');
+  league_dict['all']['satisfaction'] = await response.text();
+
+  response = await fetch('./data/tables/league-satisfaction-urge.html');
+  league_dict['urge']['satisfaction'] = await response.text();
+
+  response = await fetch('./data/tables/league-satisfaction-ow.html');
+  league_dict['ow']['satisfaction'] = await response.text();
 
   var cy = cytoscape({
     container: document.getElementById('cy'), // container to render in
@@ -23,10 +100,10 @@ const request = async () => {
   var type = 'all';
   var outcome = [ 'cure', 'improvement', 'satisfaction' ];
 
+  //update cy elements from the elements dictionary 
   const update_nodes = () => {
     var new_nodes = [];
     var new_edges = [];
-    console.log(type);
     jQuery.each(outcome, function() {
       var o = this;
       jQuery.each(ow, function() {
@@ -35,12 +112,14 @@ const request = async () => {
         new_edges = new_edges.concat(elements[type][w][o]['edges']);
       });
     });
-    cy.json( { elements: { nodes: new_nodes, edges: new_edges }, style: elements['style'] } );
+    cy.json( { elements: { nodes: new_nodes, edges: new_edges } } );
   }
-  cy.json(node_info['all']);
+  cy.json(node_info);
   update_nodes();
 
+  //returns the studies for 2 nodes
   const get_studies_2 = ( node1, node2 ) => {
+    a_dict = { "stress": ["sui"], "urge": ["uui"], "all": ["sui","uui","0"] }
     node1_id = node1.id();
     node2_id = node2.id();
     var study_list = [];
@@ -48,27 +127,51 @@ const request = async () => {
       if ((this['coarse_trt_code1'] == node1_id &&
         this['coarse_trt_code2'] == node2_id) ||
         (this['coarse_trt_code1'] == node2_id &&
-        this['coarse_trt_code2'] == node1_id )) {
+        this['coarse_trt_code2'] == node1_id) && 
+        ( a_dict[type].indexOf(this['ui_type'].toString()) >= 0 ) && 
+        ( ow.indexOf(this['older_women'].toString()) >= 0 ) &&
+        ( outcome.indexOf(this['outcome'].toString()) >= 0 )) {
           study_list.push(this);
       }
     });
     return study_list;
   }
 
+  //returns the studies for a single node
   const get_studies_1 = ( node ) => {
+    a_dict = { "stress": ["sui"], "urge": ["uui"], "all": ["sui","uui","0"] }
     node_id = node.id();
     var study_list = [];
     jQuery.each(graph_data, function() {
-      if ( this['coarse_trt_code1'] == node_id ||
-        this['coarse_trt_code2'] == node_id ) {
+      if (( this['coarse_trt_code1'] == node_id ||
+        this['coarse_trt_code2'] == node_id ) && 
+        ( a_dict[type].indexOf(this['ui_type'].toString()) >= 0 ) && 
+        ( ow.indexOf(this['older_women'].toString()) >= 0 ) &&
+        ( outcome.indexOf(this['outcome'].toString()) >= 0 )) {
           study_list.push(this);
+      }    });
+    return study_list;
+  }
+
+  //returns the studies for the whole graph
+  const get_studies_0 = () => {
+    a_dict = { "stress": ["sui"], "urge": ["uui"], "all": ["sui","uui","0"] }
+    var study_list = [];
+    jQuery.each(graph_data, function() {
+      if (( a_dict[type].indexOf(this['ui_type'].toString()) >= 0 ) && 
+        ( ow.indexOf(this['older_women'].toString()) >= 0 ) &&
+        ( outcome.indexOf(this['outcome'].toString()) >= 0 )) {
+          study_list.push(this);
+      }else{
+        console.log(this);
       }
     });
+    console.log(graph_data.length, study_list.length);
     return study_list;
   }
 
 
-
+  //when a node is clicked on, update the current and previous nodes
   cy.on('tap', 'node', function(e) {
     if ( e.target == prev_node ) {
       prev_node.removeClass('highlight');
@@ -96,6 +199,7 @@ const request = async () => {
   });
 
 
+  //update the cy elements array when filtering settings are changed
   $('#type-form input').on('change', function() {
     node = null;
     prev_node = null;
@@ -115,6 +219,7 @@ const request = async () => {
     update_nodes();
   });
 
+  //update the cy elements array when filtering settings are changed
   $('#ow-form input').on('change', function() {
     node = null;
     prev_node = null;
@@ -136,19 +241,22 @@ const request = async () => {
 
   $('input[name=outcome-checkbox]').change(
     function(){
+      node = null;
+      prev_node = null;
+
       if ($(this).is(':checked')) {
         outcome.push($(this).val());
       } else {
         outcome.splice(outcome.indexOf($(this).val()),1);
       }
-      if (outcome.length > 1) {
-        $('#summary-button').attr('disabled', true); 
-        $('#absolute-effects-button').attr('disabled', true); 
-        $('#relative-effects-button').attr('disabled', true); 
+      if (outcome.length == 1) {
+        //$('#summary-button').attr('disabled', false); 
+        $('#league-button').attr('disabled', false); 
+        $('#relative-button').attr('disabled', false); 
       } else {
-        $('#summary-button').attr('disabled', false); 
-        $('#absolute-effects-button').attr('disabled', false); 
-        $('#relative-effects-button').attr('disabled', false); 
+        $('#summary-button').attr('disabled', true); 
+        $('#league-button').attr('disabled', true); 
+        $('#relative-button').attr('disabled', true); 
       }
       update_nodes();
     });
@@ -156,87 +264,86 @@ const request = async () => {
   //button that pulls the studies for nodes
   $('#get-studies-button').on('click', function() {
     var study_list;
-    if (prev_node == null) {
+    if (node == null) {
+      study_list = get_studies_0();
+    } else if (prev_node == null) {
       study_list = get_studies_1(node);
     } else {
       study_list = get_studies_2(prev_node, node);
     }
 
     $('#studies-modal-content').empty();
-    content = "<table>";
-    content += "<tr>";
+    content = "<table id='studies-table' class='display nowrap'>";
+    content += "<thead><tr align='left'>";
     content += "<th> PMID </th>";
-    content += "<th> Study_Author </th>";
+    content += "<th> Study Author </th>";
     content += "<th> Year </th>";
     content += "<th> Age </th>";
     content += "<th> Older_women </th>";
-    content += "<th> UI_type </th>";
-    content += "<th> followup </th>";
-    content += "<th> n1 </th>";
-    content += "<th> n2 </th>";
-    content += "<th> N1 </th>";
-    content += "<th> N2 </th>";
-    content += "<th> granular_trt_code1 </th>";
-    content += "<th> granular_trt_code2 </th>";
-    content += "<th> coarse_trt_code1 </th>";
-    content += "<th> coarse_trt_code2 </th>";
-    content += "<th> trt_description1 </th>";
-    content += "<th> trt_description2 </th>";
-    content += "<th> OR12 </th>";
-    content += "<th> OR21 </th>";
+    content += "<th> UI type </th>";
+    content += "<th> Followup </th>";
+    content += "<th> Counts </th>";
+    content += "<th> Coarse trt </th>";
+    content += "<th> Granular trt </th>";
+    content += "<th> trt description1 </th>";
+    content += "<th> trt description2 </th>";
+    content += "<th> OR </th>";
     content += "<th> Outcome </th>";
-    content += "<th> combined </th>";
-    content += "<th> Blinding_of_Patients </th>";
-    content += "<th> Blinding_of_Assessors </th>";
-    content += "<th> Intention_to_Treat </th>";
-    content += "<th> Attrition_Bias </th>";
-    content += "<th> Selection_Bias </th>";
-    content += "<th> Interventions_Described </th>";
-    content += "<th> Intervention_Compliance </th>";
-    content += "<th> Other_Issues </th>";
-    content += "<th> Allocation_Concealment </th>";
-    content += "</tr>";
+    content += "<th> Combined </th>";
+    content += "<th> Blinding of Patients </th>";
+    content += "<th> Blinding of Assessors </th>";
+    content += "<th> Intention to Treat </th>";
+    content += "<th> Attrition Bias </th>";
+    content += "<th> Selection Bias </th>";
+    content += "<th> Intervention Described </th>";
+    content += "<th> Intervention Compliance </th>";
+    content += "<th> Other Issues </th>";
+    content += "<th> Allocation Concealment </th>";
+    content += "</tr></thead>";
+    content += "<tbody>"
     jQuery.each(study_list, function() {
       content += '<tr>';
-      content += '<td>' + this['PMID'] + '</td>';
-      content += '<td>' + this['Study_Author'] + '</td>';
-      content += '<td>' + this['Year'] + '</td>';
-      content += '<td>' + this['Age'] + '</td>';
-      content += '<td>' + this['Older_women'] + '</td>';
-      content += '<td>' + this['UI_type'] + '</td>';
+      content += '<td>' + this['pmid'] + '</td>';
+      content += '<td>' + this['study_author'] + '</td>';
+      content += '<td>' + this['year'] + '</td>';
+      content += '<td>' + this['age'] + '</td>';
+      content += '<td>' + this['older_women'] + '</td>';
+      content += '<td>' + this['ui_type'] + '</td>';
       content += '<td>' + this['followup'] + '</td>';
-      content += '<td>' + this['n1'] + '</td>';
-      content += '<td>' + this['n2'] + '</td>';
-      content += '<td>' + this['N1'] + '</td>';
-      content += '<td>' + this['N2'] + '</td>';
-      content += '<td>' + this['granular_trt_code1'] + '</td>';
-      content += '<td>' + this['granular_trt_code2'] + '</td>';
-      content += '<td>' + this['coarse_trt_code1'] + '</td>';
-      content += '<td>' + this['coarse_trt_code2'] + '</td>';
+      content += '<td>' + this['counts_string'] + '</td>';
+      content += '<td>' + this['coarse_comparison_string'] + '</td>';
+      content += '<td>' + this['granular_comparison_string'] + '</td>';
       content += '<td>' + this['trt_description1'] + '</td>';
       content += '<td>' + this['trt_description2'] + '</td>';
-      content += '<td>' + this['OR12'] + '</td>';
-      content += '<td>' + this['OR21'] + '</td>';
-      content += '<td>' + this['Outcome'] + '</td>';
+      content += '<td>' + this['or_string'] + '</td>';
+      content += '<td>' + this['outcome'] + '</td>';
       content += '<td>' + this['combined'] + '</td>';
-      content += '<td>' + this['Randomization_Sequence'] + '</td>';
-      content += '<td>' + this['Blinding_of_Assessors'] + '</td>';
-      content += '<td>' + this['Intention_to_Treat'] + '</td>';
-      content += '<td>' + this['Attrition_Bias'] + '</td>';
-      content += '<td>' + this['Selection_Bias'] + '</td>';
-      content += '<td>' + this['Interventions_Described'] + '</td>';
-      content += '<td>' + this['Intervention_Compliance'] + '</td>';
-      content += '<td>' + this['Other_Issues'] + '</td>';
-      content += '<td>' + this['Allocation_Concealment'] + '</td>';
+      content += '<td>' + this['randomization_sequence'] + '</td>';
+      content += '<td>' + this['blinding_of_assessors'] + '</td>';
+      content += '<td>' + this['intention_to_treat'] + '</td>';
+      content += '<td>' + this['attrition_bias'] + '</td>';
+      content += '<td>' + this['selection_bias'] + '</td>';
+      content += '<td>' + this['interventions_described'] + '</td>';
+      content += '<td>' + this['intervention_compliance'] + '</td>';
+      content += '<td>' + this['other_issues'] + '</td>';
+      content += '<td>' + this['allocation_concealment'] + '</td>';
       content += '</tr>';
     });
+    content += '</tbody>';
     content += '</table>';
-    $('#studies-modal-content').append(content);
+    $('#studies-modal-content').html(content);
     $('#cy').hide();
     $('#navbar').hide();
     $('#sidebar').hide();
     $('#legend').hide();
     $('#studies-modal').show();
+    $('#studies-table').DataTable({
+        autoWidth: true,
+        pageLength: 15,
+        scrollX: true,
+        dom: 'Bfrtip',
+        buttons: [ 'colvis' ]
+      });
   });
   $('#close-studies-modal').on('click', function() {
     $('#cy').show();
@@ -273,6 +380,37 @@ const request = async () => {
     $('#legend').show();
     $('#overview-dummy').hide();
   });
+  $('#close-table').on('click', function() {
+    $('#cy').show();
+    $('#navbar').show();
+    $('#sidebar').show();
+    $('#legend').show();
+    $('#table-div').hide();
+  });
+  $('#league-button').on('click', function() {
+    $('#table-content').html(league_dict[type][outcome[0]]);
+    
+    $('#cy').hide();
+    $('#navbar').hide();
+    $('#sidebar').hide();
+    $('#legend').hide();
+    $('#table-div').show();
+  });
+
+  $('#relative-button').on('click', function() {
+    if ( ow == ['1'] && type == 'all' && outcome == ['all'] ){
+      $('#table-content').html(relative_effectiveness_dict['ow']['all']); 
+    } else { 
+      $('#table-content').html(relative_effectiveness_dict[type][outcome[0]]);
+    }
+
+    $('#cy').hide();
+    $('#navbar').hide();
+    $('#sidebar').hide();
+    $('#legend').hide();
+    $('#table-div').show();
+  });
+
 }
 
 request();
